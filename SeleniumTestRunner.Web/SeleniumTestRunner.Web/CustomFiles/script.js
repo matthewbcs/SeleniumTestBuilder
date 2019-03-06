a0987f5b2ac5657353b85de3ba186eb2e299eedf
matthewbcs <matthewbcs@hotmail.co.uk>
2019-03-06 23:14:25 +0000

    first commit

---
+++ SeleniumTestRunner.Web/SeleniumTestRunner.Web/CustomFiles/script.js
@@ -0,0 +1,34 @@
+﻿angular.module('myapp', ['psi.sortable']);
+
+function ctrl($scope) {
+    setTimeout(function () {
+        $scope.$apply(function () {
+
+            var step = {
+                stepType: '',
+                stepDetail: '',
+                param1: '',
+                param2: '',
+            };
+
+           // $scope.list = ['first', 'second', 'third', 'last'];
+           $scope.stepTypeList = ['Given', 'When', 'Then'];
+            $scope.stepDetailList = ['I go to Url', 'I click on selector', 'this selector is visible on the page', ];
+            $scope.list = [step];
+        });
+
+       
+
+    }, 500);
+
+    $scope.addStep = function () {
+        var step = {
+            stepType: '',
+            stepDetail: '',
+            param1: '',
+            param2: '',
+        };
+
+        $scope.list.push(step);
+    };
+}
